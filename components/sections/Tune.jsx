4cf62a3736bea10baf40ebb143774d7ff23945fc
Siam Al Sahariar <siam.it.int@gmail.com>
2024-12-22 18:59:06 +0600

    Added animation home page

--- components/sections/Tune.jsx
+++ components/sections/Tune.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { PlayerStore } from "../../store"; 
-import styles from "./Tune.module.css"; 
+import classNames from "classnames";
+import { PlayerStore } from "../../store";
+import Pulse from "../utils/PulseSecondary";
+import styles from "./Tune.module.css";
 
 const Tune = () => {
-  const playing = PlayerStore.useState((s) => s.playing); 
+  const reciterImage = PlayerStore.useState((s) => s.reciterImage);
+  const playing = PlayerStore.useState((s) => s.playing);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.icon}>
-        <div
-          className={`${styles.visualize} ${playing ? styles.playing : styles.notPlaying}`}
-        >
-          <span className={`${styles.spike} ${styles.spike1}`}></span>
-          <span className={`${styles.spike} ${styles.spike2}`}></span>
-          <span className={`${styles.spike} ${styles.spike3}`}></span>
-          <span className={`${styles.spike} ${styles.spike4}`}></span>
-          <span className={`${styles.spike} ${styles.spike5}`}></span>
-          <span className={`${styles.spike} ${styles.spike6}`}></span>
-          <span className={`${styles.spike} ${styles.spike7}`}></span>
+        <div className={styles.visualize}>
+          <span className={classNames(styles.spike, styles.spike1)}></span>
+          <span className={classNames(styles.spike, styles.spike2)}></span>
+          <span className={classNames(styles.spike, styles.spike3)}></span>
+          <span className={classNames(styles.spike, styles.spike4)}></span>
+          <span className={classNames(styles.spike, styles.spike5)}></span>
+          <span className={classNames(styles.spike, styles.spike6)}></span>
+          <span className={classNames(styles.spike, styles.spike7)}></span>
         </div>
       </div>
       <div className={styles.outer}></div>
