ccd2affa3aaaaceb0914a42350eb7b5f53c2e6ae
siamshariar <siam.it.int@gmail.com>
2026-08-01 22:46:03 +0600

    Update AllChapters.jsx

--- components/pages/AllChapters.jsx
+++ components/pages/AllChapters.jsx
@@ -18,23 +18,23 @@ const AllChapterList = ({ chapters }) => {
 	return (
 		<>
 			<div className={styles.content} style={contentStyle}>
-				<div className="page_width">
-					{isReady && (
-						<Virtuoso
+			<div className="page_width" style={{ paddingBottom: "100px" }}>
+			{isReady && (
+				<Virtuoso
 							style={{ height: '100%' }}
-							totalCount={chapters.length}
+				totalCount={chapters.length}
 							useWindowScroll
-							itemContent={(index) => (
-							<AllChapterCard
-								key={chapters[index].id}
-								index={index}
-	              chapter={chapters[index]}
-	            />
-							)}
-          overscan={200}
-        />
-					)}
-				</div>
+				itemContent={(index) => (
+					<AllChapterCard
+					key={chapters[index].id}
+					index={index}
+					chapter={chapters[index]}
+					/>
+				)}
+				overscan={200}
+				/>
+			)}
+			</div>
 			</div>
 		</>
 	);
