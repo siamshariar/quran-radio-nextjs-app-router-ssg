beb8258b4887d626a4b86ba0051d5ac75ad038f3
Siam Al Sahariar <siam.it.int@gmail.com>
2026-09-20 14:34:42 +0600

    Reword reload guide tooltip to reflect reload, not shuffle

--- components/actions/RandomPlay.jsx
+++ components/actions/RandomPlay.jsx
@@ -73,7 +73,7 @@ const RandomPlay = ({ classes = {} }) => {
 		<div className={`${classes.root} ${styles.root}`}>
 			{showGuide && (
 				<div className={styles.tooltip}>
-					Tap to shuffle and play a random {mode === "normal" ? "surah" : "live radio"}
+					Click to reload and randomly play a {mode === "normal" ? "surah" : "live radio"}
 					<div className={styles.tooltip_arrow}></div>
 				</div>
 			)}
